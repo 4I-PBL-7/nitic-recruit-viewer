ca3d0969a23199ce9674d34d6c79237c3b8cf666
ko50 <kotyana1331@gmail.com>
2022-11-25 14:30:28 +0900

    [変更] リンクにできるようにした

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx'
+import Link from 'next/link'
 import styles from 'styles/components/Text.module.css'
 
 type FontSize = 'header' | 'heading' | 'caption' | 'body' | 'small'
@@ -8,15 +9,32 @@ type Props = {
   fontSize?: FontSize
   fontWeight?: FontWeight
   className?: string
+  href?: string
 }
 
 export const Text: React.FC<Props> = ({
   text,
   fontSize = 'body',
   fontWeight = 'normal',
+  href,
   className,
 }) => {
-  return (
+  return href !== undefined ? (
+    <Link href={href} passHref>
+      <a>
+        <span
+          className={clsx(
+            styles[fontSize],
+            styles[fontWeight],
+            styles.underline,
+            className
+          )}
+        >
+          {text}
+        </span>
+      </a>
+    </Link>
+  ) : (
     <span className={clsx(styles[fontSize], styles[fontWeight], className)}>
       {text}
     </span>
