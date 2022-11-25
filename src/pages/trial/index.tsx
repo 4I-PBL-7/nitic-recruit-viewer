b1191e3124f840c4a899fd7fe3849e32bb847ddc
ko50 <kotyana1331@gmail.com>
2022-11-25 14:40:58 +0900

    [変更] trialの見た目を変更

--- src/pages/trial/index.tsx
+++ src/pages/trial/index.tsx
@@ -9,15 +9,18 @@ import { Card3 } from 'components/trial/Card3'
 const Trial: NextPage = () => {
   return (
     <main className={styles.main}>
-      <Text
-        text="テスト"
-        fontSize="header"
-        fontWeight="black"
-        href="https://example.com"
-      />
-      <Card1 /> {/* TODO: タイトルや人数を追加 (田口) */}
-      <Card2 /> {/* TODO: タイトルや人数を追加 (トゥグスー) */}
-      <Card3 /> {/* TODO: タイトルや人数を追加 (水流添) */}
+      <div>
+        <Text text="田口" fontSize="caption" />
+        <Card1 /> {/* TODO: タイトルや人数を追加 (田口) */}
+      </div>
+      <div>
+        <Text text="トゥグスー" fontSize="caption" />
+        <Card2 /> {/* TODO: タイトルや人数を追加 (トゥグスー) */}
+      </div>
+      <div>
+        <Text text="水流添" fontSize="caption" />
+        <Card3 /> {/* TODO: タイトルや人数を追加 (水流添) */}
+      </div>
     </main>
   )
 }
