9a8ebc76b24d346d10ca2c461b70c824a7cb9be1
ko50 <kotyana1331@gmail.com>
2022-12-09 15:06:28 +0900

    [変更] 背景色など & フォルダ名のミスを修正

--- src/pages/collage-history/index.tsx
+++ src/pages/collage-history/index.tsx
@@ -2,18 +2,19 @@ import { Header } from 'components/CollageHeader'
 import { CollageCard } from 'components/CollageCard'
 import { SearchCollageForm } from 'components/SearchCollageForm'
 import { NextPage } from 'next'
+import styles from 'styles/pages/collage-history/index.module.css'
 
 type Props = {}
 
 const SearchCollageHistory: NextPage<Props> = () => {
   return (
-    <div>
+    <main className={styles.main}>
       <Header title="茨城高専 過去の進学先" />
       <div>
         <SearchCollageForm />
         <CollageCard />
       </div>
-    </div>
+    </main>
   )
 }
 
--- src/pages/job-history/index.tsx
+++ src/pages/job-history/index.tsx
@@ -2,18 +2,19 @@ import { Header } from 'components/CollageHeader'
 import { JobCard } from 'components/JobCard'
 import { SearchJobForm } from 'components/SearchJobForm'
 import { NextPage } from 'next'
+import styles from 'styles/pages/job-history/index.module.css'
 
 type Props = {}
 
 const SearchJobHistory: NextPage<Props> = () => {
   return (
-    <div>
+    <main className={styles.main}>
       <Header title="茨城高専 過去の就職先" />
       <div>
         <SearchJobForm />
         <JobCard />
       </div>
-    </div>
+    </main>
   )
 }
 
