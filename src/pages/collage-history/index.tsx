9d274585b9d1490b13f75d68cf61da54cc695fc6
ko <60608740+ko50@users.noreply.github.com>
2022-12-09 15:30:42 +0900

    [変更] `/job-history` ページと `/collage-history` ページのレイアウト (#5)
    * [変更] 背景色など & フォルダ名のミスを修正
    
    * [追加] ページ内のレイアウト
    
    * [追加] 間の余白
    
    * [追加] テキストを追加

--- src/pages/collage-history/index.tsx
+++ src/pages/collage-history/index.tsx
@@ -2,18 +2,23 @@ import { Header } from 'components/CollageHeader'
 import { CollageCard } from 'components/CollageCard'
 import { SearchCollageForm } from 'components/SearchCollageForm'
 import { NextPage } from 'next'
+import styles from 'styles/pages/collage-history/index.module.css'
+import { Text } from 'components/Text'
 
 type Props = {}
 
 const SearchCollageHistory: NextPage<Props> = () => {
   return (
-    <div>
+    <main className={styles.main}>
       <Header title="茨城高専 過去の進学先" />
-      <div>
+      <div className={styles.content}>
         <SearchCollageForm />
-        <CollageCard />
+        <div>
+          <Text text="検索結果" fontSize="caption" />
+          <CollageCard />
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
--- src/pages/job-history/index.tsx
+++ src/pages/job-history/index.tsx
@@ -1,19 +1,24 @@
 import { Header } from 'components/CollageHeader'
 import { JobCard } from 'components/JobCard'
 import { SearchJobForm } from 'components/SearchJobForm'
+import { Text } from 'components/Text'
 import { NextPage } from 'next'
+import styles from 'styles/pages/job-history/index.module.css'
 
 type Props = {}
 
 const SearchJobHistory: NextPage<Props> = () => {
   return (
-    <div>
+    <main className={styles.main}>
       <Header title="茨城高専 過去の就職先" />
-      <div>
+      <div className={styles.content}>
         <SearchJobForm />
-        <JobCard />
+        <div>
+          <Text text="検索結果" fontSize="caption" />
+          <JobCard />
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
