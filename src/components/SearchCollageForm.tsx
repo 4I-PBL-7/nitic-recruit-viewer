6422d1bc89312bde1122f1922ed357c6ae362be8
ko50 <kotyana1331@gmail.com>
2022-12-09 13:56:01 +0900

    [追加] フォームの雛形も追加

---
+++ src/components/SearchCollageForm.tsx
@@ -0,0 +1,7 @@
+import styles from 'styles/components/SearchCollageForm.module.css'
+
+type Props = {}
+
+export const SearchCollageForm: React.FC<Props> = ({}) => {
+  return <div></div>
+}
---
+++ src/components/SearchJobForm.tsx
@@ -0,0 +1,7 @@
+import styles from 'styles/components/SearchJobForm.module.css'
+
+type Props = {}
+
+export const SearchJobForm: React.FC<Props> = ({}) => {
+  return <div></div>
+}
